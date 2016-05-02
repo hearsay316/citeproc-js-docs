4353f341faf722d9d133d75bae3eee1e550b772d
fbennett <biercenator@gmail.com>
2016-05-03 08:36:39 +0900

    Give up on touch. It's a demo.

--- _static/workerapi/cslui.js
+++ _static/workerapi/cslui.js
@@ -138,33 +138,4 @@ window.addEventListener('load', function() {
             button.addEventListener('click', removeCiteMenu);
         }
     });
-    document.body.addEventListener('touch', function(evt) {
-        evt.preventDefault();
-        if (evt.touches.length > 1 || (evt.type == "touchend" && evt.touches.length > 0))
-            return;
-        
-        var newEvt = document.createEvent("MouseEvents");
-        var type = null;
-        var touch = null;
-        
-        switch (evt.type) {
-        case "touchstart": 
-            type = "mousedown";
-            touch = evt.changedTouches[0];
-            break;
-        case "touchmove":
-            type = "mousemove";
-            touch = evt.changedTouches[0];
-            break;
-        case "touchend":        
-            type = "mouseup";
-            touch = evt.changedTouches[0];
-            break;
-        }
-        
-        newEvt.initMouseEvent(type, true, true, evt.originalTarget.ownerDocument.defaultView, 0,
-                              touch.screenX, touch.screenY, touch.clientX, touch.clientY,
-                              evt.ctrlKey, evt.altKey, evt.shiftKey, evt.metaKey, 0, null);
-        evt.originalTarget.dispatchEvent(newEvt);
-    });
 });
